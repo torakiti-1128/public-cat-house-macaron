7ebc10506b10bf9664792968a30e450f3d8ebc12
torakiti-1128 <129305136+torakiti-1128@users.noreply.github.com>
2024-11-02 23:00:44 +0900

    Remove unused variables

--- src/app/kittens/[id]/page.tsx
+++ src/app/kittens/[id]/page.tsx
@@ -1,46 +1,49 @@
-'use client';
+// 'use client';
 // import { fetchKittenDetail, fetchKittenMediaUrls } from "@/api/kitten/detail";
 // import { KittenDetailType, KittenMediaType } from "@/types/kitten";
 // import React, { useEffect, useState } from "react";
 
-interface Params {
-    id: string; // ここでparamsの型を定義
-  }
+// interface Params {
+//     id: string; // ここでparamsの型を定義
+//   }
 
-export default function KittenDetailPage({ params }: { params: Params }){
-    // const [kittens, setKittens] = useState<KittenDetailType | null>(null);
-    // const [kittensMedia, setKittensMedia] = useState<KittenMediaType[] | null>(null);
-    // const kittenId = parseInt(params.id, 10);
+// export default function KittenDetailPage({ params }: { params: Params }){
+//     const [kittens, setKittens] = useState<KittenDetailType | null>(null);
+//     const [kittensMedia, setKittensMedia] = useState<KittenMediaType[] | null>(null);
+//     const kittenId = parseInt(params.id, 10);
 
-    // useEffect(() => {
-    //     const fetchKittens = async () => {
-    //         try {
-    //             const data: KittenDetailType | null = await fetchKittenDetail(kittenId);
-    //             setKittens(data);
-    //         } 
-    //         catch (err) {
-    //             console.log(err);
-    //         } 
-    //     };
+//     useEffect(() => {
+//         const fetchKittens = async () => {
+//             try {
+//                 const data: KittenDetailType | null = await fetchKittenDetail(kittenId);
+//                 setKittens(data);
+//             } 
+//             catch (err) {
+//                 console.log(err);
+//             } 
+//         };
 
-    //     fetchKittens();
-    // }, [kittenId]);
+//         fetchKittens();
+//     }, [kittenId]);
 
-    // useEffect(() => {
-    //     const fetchKittensMedia = async () => {
-    //         try {
-    //             const data: KittenMediaType[] | null = await fetchKittenMediaUrls(kittenId);
-    //             setKittensMedia(data);
-    //         } 
-    //         catch (err) {
-    //             console.log(err);
-    //         } 
-    //     };
+//     useEffect(() => {
+//         const fetchKittensMedia = async () => {
+//             try {
+//                 const data: KittenMediaType[] | null = await fetchKittenMediaUrls(kittenId);
+//                 setKittensMedia(data);
+//             } 
+//             catch (err) {
+//                 console.log(err);
+//             } 
+//         };
 
-    //     fetchKittensMedia();
-    // }, [kittenId]);
+//         fetchKittensMedia();
+//     }, [kittenId]);
 
-    return(
-        <></>
-    )
+//     return(
+//         <></>
+//     )
+// }
+
+export default function KittenDetailPage(){
 }
