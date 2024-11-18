5b0f11a97e198f8fc9b2668c29007f3dcf7d7f64
torakiti-1128 <129305136+torakiti-1128@users.noreply.github.com>
2024-11-18 13:24:21 +0900

    変更忘れ発見 jpg → JPG

--- src/app/kittens/page.tsx
+++ src/app/kittens/page.tsx
@@ -12,7 +12,7 @@ const testKittens: KittenListType[] = [
   { kittenId: 1, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat1.JPG" },
   { kittenId: 2, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat2.JPG" },
   { kittenId: 3, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat3.JPG" },
-  { kittenId: 4, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat4.JPG" },
+  { kittenId: 4, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat10.JPG" },
   { kittenId: 5, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat5.JPG" },
   { kittenId: 6, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat6.JPG" },
   { kittenId: 7, breed: "スコティッシュフィールド", tranStatus: "", url: "/images/cats/cat7.JPG" },
