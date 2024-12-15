3b1a6f4645ac3b00c221abae6061deff48382324
torakiti-1128 <129305136+torakiti-1128@users.noreply.github.com>
2024-12-15 19:43:51 +0900

    エイリアス修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Header from '../components/layout/Header'
-import CheckPoints from '../components/layout/CheckPoints'
-import Access from '../components/layout/Access'
-import Footer from '../components/layout/Footer'
 import '../styles/globals.css'
+import Header from '@/components/layout/Header'
+import CheckPoints from '@/components/layout/CheckPoints'
+import Access from '@/components/layout/Access'
+import Footer from '@/components/layout/Footer'
 
 export const metadata = {
   title: 'Cat House Macaron',
