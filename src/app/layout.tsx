8965859d125738240cb3b7dea09f34483063ef69
torakiti-1128 <129305136+torakiti-1128@users.noreply.github.com>
2024-12-28 16:29:52 +0900

    layoutフォルダの名前変更、layout.tsxとの競合の可能性がある

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../styles/globals.css'
-import Header from '@/components/layout/Header'
-import CheckPoints from '@/components/layout/CheckPoints'
-import Access from '@/components/layout/Access'
-import Footer from '@/components/layout/Footer'
+import Header from '@/components/shared/Header'
+import CheckPoints from '@/components/shared/CheckPoints'
+import Access from '@/components/shared/Access'
+import Footer from '@/components/shared/Footer'
 
 export const metadata = {
   title: 'Cat House Macaron',
