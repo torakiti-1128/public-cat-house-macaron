19df2dca8b4860d6ee52df7e66b74a7e4f9495b9
torakiti-1128 <129305136+torakiti-1128@users.noreply.github.com>
2025-04-29 04:22:05 +0900

    Reservation.tsx を更新

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -20,7 +20,7 @@ const Reservation: React.FC = () => {
                 <p className="leading-relaxed mb-4">{item.content}</p>
               )}
               {item.list && (
-                <ul className="list-disc pl-6 space-y-2">
+                <ul className="list-disc pl-6 space-y-2 mb-4">
                   {item.list.map((listItem, idx) => (
                     <li key={idx}>{listItem}</li>
                   ))}
